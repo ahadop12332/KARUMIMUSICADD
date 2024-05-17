bf75a48901f62b3a2c7947f42494ce7872b4f510
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-05-17 09:39:25 +0530

    Update __init__.py

--- DAXXMUSIC/__init__.py
+++ DAXXMUSIC/__init__.py
@@ -4,7 +4,7 @@ from DAXXMUSIC.core.git import git
 from DAXXMUSIC.core.userbot import Userbot
 from DAXXMUSIC.misc import dbb, heroku
 
-from SafoneAPI import SafoneAPI
+#from SafoneAPI import SafoneAPI
 from .logging import LOGGER
 
 dirr()
@@ -13,7 +13,7 @@ dbb()
 heroku()
 
 app = DAXX()
-api = SafoneAPI()
+#api = SafoneAPI()
 userbot = Userbot()
 
 
