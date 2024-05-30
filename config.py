c100b7b89b5ba846b281f8fde4d8dccdd8720026
Honeyxcoder <160338822+HONEYXCODER@users.noreply.github.com>
2024-05-30 20:32:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Sunnykumar1122/BABYMUSIC",
+    "https://github.com/HONEYXCODER/KARUMIMUSIC.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
@@ -141,21 +141,21 @@ confirmer = {}
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/cfbdee8103102bcb2e5da.jpg"
+    "START_IMG_URL", "https://graph.org/file/2af9a3de25e5ac7d11eac.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/00360393a15daf7fc4e9d.jpg"
+    "PING_IMG_URL", "https://graph.org/file/2af9a3de25e5ac7d11eac.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/0c85a395dcf8132c3ecf8.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/48f39202823b358203234.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/e575ae40d6635250974e1.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/03efec694e41e891b29dc.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/4dc854f961cd3ce46899b.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/b32f6f20a6a202110b8f5.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/b32f6f20a6a202110b8f5.jpg"
+STREAM_IMG_URL = "https://graph.org/file/f6cfc1542d7e8dcd6225d.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/f6cfc1542d7e8dcd6225d.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/2c20f0b38e7f5364b1303.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/2c20f0b38e7f5364b1303.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/9481cfcfbd3208ffb646d.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/9481cfcfbd3208ffb646d.jpg"
 
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
